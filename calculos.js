1735631a6adf301b94d6d4044883f0a4162a5046
Richard <ricardo.elizaldeh@un.edu.mx>
2025-11-04 09:53:15 -0600

    Update calculos.js

--- calculos.js
+++ calculos.js
@@ -1,6 +1,31 @@
-var formulario = document.getElementById("cantidad").value;
+var formulario = document.getElementById("calcularPrecio");
 
 function Calcular() {
     //alert("Si funciona mi botón");
-    alert(formulario);
+
+    var cantidad = document.getElementById("cantidad").value;
+    
+    //var tamanio = document.getElementsByName("tam").value;
+
+    //var tamChico = document.getElementById("chico");
+
+    var tamanio = formulario.tam.value;
+    alert(tamanio);
+
+    var total = 0;
+
+    if(tamanio == "chico")
+    {
+        total = 100;
+    }
+    else if (tamanio == "mediano")
+    {
+        total = 150;
+    }
+    else if (tamanio == "grande")
+    {
+        total = 200;
+    }
+
+    formulario.total.value = total;
 }
